ee7d38fe32eb796730ebe87b95fb29ca7b50a7b0
Ali MIJIYAWA <60366981+Alimiji@users.noreply.github.com>
2025-04-23 12:40:48 -0400

    Update monitor_model.py

--- src/monitoring/monitor_model.py
+++ src/monitoring/monitor_model.py
@@ -1,1 +1,19 @@
 # Scripts de suivi du modèle (drift, alertes)
+
+import pandas as pd
+import joblib
+from sklearn.metrics import accuracy_score
+
+def monitor():
+    model = joblib.load("models/model.pkl")
+    df = pd.read_csv("data/processed/monitor.csv")
+    X = df.drop("target", axis=1)
+    y_true = df["target"]
+    y_pred = model.predict(X)
+
+    acc = accuracy_score(y_true, y_pred)
+    print(f"🧪 Current model accuracy on live data: {acc:.4f}")
+
+if __name__ == "__main__":
+    monitor()
+
